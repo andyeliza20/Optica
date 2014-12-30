253ebd1fc3edebcbf6c8b21794ff4109ec775404
Gabow135 <jgabrielreyesv@gmail.com>
2014-12-29 21:10:58 -0500

    Agregando Proyecto
    Diciembre 29, 2014

---
+++ Optica/Login.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Optica.Login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Optica Andina</title>
+    <link href="Content/StyleLogin.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<br /><br /><br /><br /><br /><br />
+<div class="container">
+	<section id="content">
+        <form id="form1" runat="server">
+        <h1>Optica Andina</h1>
+			<div>
+                <asp:TextBox ID="username" runat="server" placeholder="Usuario" required=""></asp:TextBox>
+			</div>
+			<div>
+                <asp:TextBox ID="password" runat="server" placeholder="Password" required="" 
+                    TextMode="Password"></asp:TextBox>
+			</div>
+			<div>
+				<asp:Button ID="Button1" runat="server" Text="Ingresar" onclick="Button1_Click" />
+				<a href="#">Olvide la contraseña?</a>
+			</div>
+            <div>
+                <asp:Label ID="lblError" runat="server" ForeColor="Red"></asp:Label>
+			</div>
+        </form>
+    </section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
---
+++ Optica/Login.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Optica
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String str = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            String query = "select * from usuarios where usuario = '" + username.Text + "'and clave = '" + this.password.Text + "'";
+            SqlConnection con = new SqlConnection(str);
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader dbr;
+            try
+            {
+                if (!(username.Text == string.Empty))
+                {
+                    if (!(password.Text == string.Empty))
+                    {
+                        con.Open();
+                        dbr = cmd.ExecuteReader();
+                        int count = 0;
+                        while (dbr.Read())
+                        {
+                            count = count + 1;
+                        }
+                        if (count == 1)
+                        {
+                            lblError.Text = "Usuario y Password Correctos";
+                            Response.Redirect("Index.aspx");
+                        }
+                        else if (count > 1)
+                        {
+                            lblError.Text = "Usuario y Password Duplicado";
+                        }
+                        else
+                        {
+                            lblError.Text = "Usuario y Password Incorrectos";
+                        }
+                    }
+                    else
+                    {
+                        lblError.Text = "Password Vacío";
+                    }
+                }
+                else
+                {
+                    lblError.Text = "Usuario Vacío";
+                }
+            }
+            catch (Exception es)
+            {
+                lblError.Text = es.Message;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
